bd19ef091984df2e3183c4711a21a816e2b2930f
alanstorquato <alan.torquato@pravaler.com.br>
2021-02-20 22:51:52 -0300

    criando servico que verifica se a transferencia esta autorizada

---
+++ app/Services/AuthorizeTransaction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class AuthorizeTransaction
+{
+    public function verifyAuthorizeTransaction()
+    {
+        $response = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6')->json();
+
+        if($response['message'] == "Autorizado"){
+            return true;
+        }
+
+        throw new \Exception('Transação não autorizada');
+    }
+
+}
