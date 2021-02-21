7140743296c258b93fc57113f1269dab80828dc2
alanstorquato <alan.torquato@pravaler.com.br>
2021-02-21 20:18:06 -0300

    implementando exception na classe de mensagem

--- app/Services/SendAuthorizeTransactionMessage.php
+++ app/Services/SendAuthorizeTransactionMessage.php
@@ -4,14 +4,19 @@ namespace App\Services;
 
 use GuzzleHttp\RetryMiddleware;
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
 
 class SendAuthorizeTransactionMessage
 {
     public function sendMessage()
     {
         $response = Http::get('https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04');
+        
+        if($response->status() == Response::HTTP_OK) {
+            return $response->json();
+        }
 
-        return $response->json();
+        throw new \Exception('Erro ao enviar mensagem');
     }
 
 }
