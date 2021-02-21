dce302984222f32dfc72e1e6f33aec9aef0eaeae
alanstorquato <alan.torquato@pravaler.com.br>
2021-02-20 22:53:00 -0300

    verificando se a transação esta atualizada e implementando DB transaction

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -3,38 +3,46 @@
 namespace App\Services;
 
 use App\Models\{Account, Transaction};
-use Exception;
-
-use function PHPUnit\Framework\throwException;
+use Illuminate\Support\Facades\DB;
 
 class TransactionService
 {
+
     public function transaction($value, $payerId, $payeeId)
     {
+        $authorizeTransaction = new AuthorizeTransaction();
 
         $payer = Account::find($payerId);
         $payee = Account::find($payeeId);
 
-        if($payer->balance >= $value){
+        if ($payer->type === 'store') {
+            throw new \Exception('Lojista não pode realizar transferencia');
+        }
 
+        if ($payer->balance >= $value) {
             $payer->balance = $payer->balance - $value;
             $payee->balance = $payee->balance + $value;
 
-            $payer->save();
-            $payee->save();
 
-            return Transaction::create(
-                [
-                   'value' => $value, 
-                   'payer_id' => $payer->id, 
-                   'payee_id' => $payee->id
-                ]
-                
-            );
+            if ($authorizeTransaction->verifyAuthorizeTransaction()) {
+                DB::transaction(function () use ($payer, $payee, $value) {
 
+                    $this->transaction =  Transaction::create(
+                        [
+                            'value' => $value,
+                            'payer_id' => $payer->id,
+                            'payee_id' => $payee->id
+                        ]
+
+                    );
+                    $payer->save();
+                    $payee->save();
+                });
+
+                return $this->transaction;
+            }
         }
-        
-        throw new Exception('Valor da transferencia maior que saldo do pagador');
 
+        throw new \Exception('Valor da transferencia maior que saldo do pagador');
     }
 }
