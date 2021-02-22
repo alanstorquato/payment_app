497811f9706eeea8f9aa88daad7914c7e504e454
alanstorquato <alan.torquato@pravaler.com.br>
2021-02-22 19:36:31 -0300

    adicionando annotataion TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -18,37 +18,62 @@ class TransactionController extends Controller
     {
         $this->transactionRepository = $transactionRepository;
         $this->accountRepository = $accountRepository;
-
     }
 
+    /**
+     * @OA\Get(
+     *     tags={"Transactions"},
+     *     path="/api/transaction",
+     *     description="Returns a Collection of Transactions",
+     *     @OA\Response(response="200", description="A list with transactions"),
+     * ),
+     *
+    */
     public function index()
     {
         return response()->json(
             $this->transactionRepository->all(),
             Response::HTTP_OK
-        ); 
+        );
     }
 
-    public function store (PostTransactionRequest $request, TransactionService $transactionService)
-    {   
+    /**
+    * @OA\Post(
+    *   tags={"Transactions"},
+    *   path="/api/transaction",
+    *   description="Create a Transactions",
+    *   @OA\RequestBody(
+    *       @OA\MediaType(mediaType="application/json",
+    *           @OA\Schema(
+    *               @OA\Property(property="value", type="number"),
+    *               @OA\Property(property="payer_id", type="integer"),
+    *               @OA\Property(property="payee_id", type="integer"),
+    *               required={"value", "payer_id", "payee_id"}
+    *           )
+    *       )
+    *   ),
+    *   @OA\Response(response="201", description="transactions created"),
+    *   @OA\Response(response="405", description="transactions not allowed")
+    * )
+    */
+    public function store(PostTransactionRequest $request, TransactionService $transactionService)
+    {
         try {
             $transction = $transactionService->transaction(
                 $request->value,
                 $this->accountRepository->find($request->payer_id),
-                $this->accountRepository->find($request->payee_id) 
+                $this->accountRepository->find($request->payee_id)
             );
-            
-        }catch(\Exception $e){
-            return response()->json( 
-                ['error' => $e->getMessage()], 
+        } catch (\Exception $e) {
+            return response()->json(
+                ['error' => $e->getMessage()],
                 Response::HTTP_METHOD_NOT_ALLOWED
             );
         }
   
-        return response()->json( 
-            [$transction], 
+        return response()->json(
+            [$transction],
             Response::HTTP_CREATED
         );
-
     }
 }
