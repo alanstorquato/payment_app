95f15adc97e16942c0814e9cc88dcb0cb5e07c14
alanstorquato <alan.torquato@pravaler.com.br>
2021-02-22 19:42:48 -0300

    criando annotations para userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,20 +16,46 @@ class UserController extends Controller
         $this->userRepository = $userRepository;
     }
 
+    /**
+     * @OA\Get(
+     *     tags={"Users"},
+     *     path="/api/user",
+     *     description="Returns a Collection of Users",
+     *     @OA\Response(response="200", description="A list with users"),
+     * ),
+     *
+    */
     public function index()
     {
         return response()->json(
             $this->userRepository->all(),
             Response::HTTP_OK
-        ); 
+        );
     }
 
-    public function store (PostUserRequest $request)
-    {   
+    /**
+    * @OA\Post(
+    *   tags={"Users"},
+    *   path="/api/user",
+    *   description="Create a Users",
+    *   @OA\RequestBody(
+    *       @OA\MediaType(mediaType="application/json",
+    *           @OA\Schema(
+    *               @OA\Property(property="name", type="string"),
+    *               @OA\Property(property="email", type="string"),
+    *               @OA\Property(property="password", type="string"),
+    *               required={"type", "name", "email", "password"}
+    *           )
+    *       )
+    *   ),
+    *   @OA\Response(response="201", description="user created")
+    * )
+    */
+    public function store(PostUserRequest $request)
+    {
         return response()->json(
             $this->userRepository->create($request->all()),
             Response::HTTP_CREATED
         );
-
     }
 }
